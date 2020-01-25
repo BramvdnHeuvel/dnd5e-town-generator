b7318c8b2e9dbde53407ad8bbd8aa1f18c86ae05
BramvdnHeuvel <git@bram.blmgroep.nl>
2020-01-25 22:27:06 +0100

    Finish namegen folder

---
+++ namegen/__init__.py
@@ -0,0 +1,35 @@
+from namegen import (
+    dragonborn,
+    dwarf,
+    elf,
+    halfelf,
+    halfling,
+    halforc,
+    human,
+    tiefling
+)
+
+RACE_BY_MODULE = {
+    'dragonborn'    :   dragonborn,
+    'dwarf'         :   dwarf,
+    'elf'           :   elf,
+    'half-elf'      :   halfelf,
+    'halfling'      :   halfling,
+    'half-orc'      :   halforc,
+    'human'         :   human,
+    'tiefling'      :   tiefling
+}
+
+def get_name_from_race(race, name_type):
+    module = RACE_BY_MODULE[race]
+
+    if name_type == 'male':
+        return module.male()
+    if name_type == 'female':
+        return module.female()
+    if name_type == 'last_name':
+        return module.last_name()
+    return None
+
+def get_race_list():
+    return [race for race in RACE_BY_MODULE]
